30a4f7ece8345b18ad772aeff853a728602cb203
Claudemir Francisco <claudemirfrancisco@gmail.com>
2024-06-16 21:16:16 -0300

    primeiro projeto react

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <>
+            <header className="banner">
+                <img src="/imagens/doAgroAoCódigo.jpg" alt="0 banner principal da página do Organos" />
+            </header>
+        </>
+    )
+}
+
